865cc1284e5e1208cff31bbcdbaa36fbd4e534b8
kunsang wangyal <kunwangyal05@gmail.com>
2016-06-30 21:04:56 +0530

    special and category layout done

--- app/src/main/java/com/netforce/ray/special_categories/SpecialAndCategoryFragment.java
+++ app/src/main/java/com/netforce/ray/special_categories/SpecialAndCategoryFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.netforce.ray.R;
 
@@ -13,7 +15,8 @@ import com.netforce.ray.R;
  * A simple {@link Fragment} subclass.
  */
 public class SpecialAndCategoryFragment extends Fragment {
-
+    ScrollView scrollView;
+    int type = 0;
 
     public SpecialAndCategoryFragment() {
         // Required empty public constructor
@@ -24,7 +27,27 @@ public class SpecialAndCategoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_special_and_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_special_and_category, container, false);
+        Bundle bundle = getArguments();
+        type=bundle.getInt("type");
+        scrollView = (ScrollView) view.findViewById(R.id.scrollView);
+        final TextView textViewSpecial,textViewCategory;
+        textViewCategory= (TextView) view.findViewById(R.id.textViewCategory);
+        textViewSpecial= (TextView) view.findViewById(R.id.textViewSpecial);
+        if (type == 0) {
+            scrollView.post(new Runnable() {
+                public void run() {
+                    scrollView.scrollTo(0, textViewSpecial.getTop());
+                }
+            });
+        } else {
+            scrollView.post(new Runnable() {
+                public void run() {
+                    scrollView.scrollTo(0, textViewCategory.getTop());
+                }
+            });
+        }
+        return view;
     }
 
 }
